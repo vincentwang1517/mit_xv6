645585192d96e0def8a9c12e1a6a68143bca641f
vincentwang1517 <vin84526@gmail.com>
2024-11-10 15:46:49 -0800

    Add sleep program

---
+++ user/sleep.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  // error handling
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep <ticks>\n");
+    exit(1);
+  }
+
+  int ticks;
+  ticks = atoi(argv[1]);
+
+  // main function
+  sleep(ticks);
+
+  exit(0);
+}
