1fe90a0f639d40ff98a290f72cac16b27220064d
vincentwang1517 <vin84526@gmail.com>
2024-11-11 21:21:36 -0800

    primes runs up to 280 by closing unnecessary fd

--- user/primes.c
+++ user/primes.c
@@ -4,14 +4,14 @@
 
 void findPrime(int) __attribute__((noreturn));
 
-int CAP = 42;
+int CAP = 280;
 
 void findPrime(int left_p)
 {
   // the first read will be the prime
   int prime;
   read(left_p, (void*)&prime, sizeof(prime));
-  fprintf(1, "prime: %d\n", prime);
+  fprintf(1, "prime %d\n", prime);
 
   // if I can read the next member (then it must be a prime)
   // then we need to create a next level process
@@ -23,25 +23,34 @@ void findPrime(int left_p)
     pipe(p);
     write(p[1], (void*)&entry, sizeof(entry));
 
+    // fork a child
     int pid;
     pid = fork();
 
     if (pid == 0) {
       close(p[1]);
+
+      // per process at maximum can have 16 file descriptors open
+      close(left_p);
+
       findPrime(p[0]);
+      close(p[0]);
     }
     else {
+      close(1);
+
       close(p[0]);
       // keep read from left nieghbor and write to right neighbor
       while (read(left_p, (void*)&entry, sizeof(entry)) != 0) {
         if (entry % prime != 0)
           write(p[1], (void*)&entry, sizeof(entry));
       }
-      close(p[1]);
 
-      close(left_p);
+      close(p[1]);
     }
   }
+
+  close(left_p);
 
   exit(0);
 }
@@ -52,14 +61,20 @@ main(int argc, char *argv[])
   int p[2];
   pipe(p);
 
+  close(0);
+  close(2);
+
   int pid;
   pid = fork();
 
   if (pid == 0) {
     close(p[1]);
     findPrime(p[0]);
+    close(p[0]);
   }
   else { // parent
+    close(1);
+
     close(p[0]);
     // send 2 - 280
     for (int i = 2; i <= CAP; i++) {
